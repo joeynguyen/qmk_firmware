b45dc4cec18c22d26899bbebd687b21ae23f422f
Joey Nguyen <joey.nguyen@slingshotaerospace.com>
2022-01-27 16:47:28 -0600

    change timing for tap/hold

--- keyboards/splitkb/kyria/keymaps/joeynguyen/config.h
+++ keyboards/splitkb/kyria/keymaps/joeynguyen/config.h
@@ -27,6 +27,9 @@
 // Lets you roll mod-tap keys
 #define IGNORE_MOD_TAP_INTERRUPT
 
+// determines what is a tap and what is a hold - default is 200ms
+#define TAPPING_TERM 175
+
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 // #define SPLIT_USB_DETECT
 // #define NO_USB_STARTUP_CHECK
